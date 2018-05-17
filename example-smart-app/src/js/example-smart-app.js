24becbf510f481143d0a5b036ffaf4851b4417ba
dcmoore182 <dmoore@trinisys.com>
2018-05-17 12:39:45 -0500

    show mrn

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -7,63 +7,65 @@
       ret.reject();
     }
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
+      function onReady(smart)  {
+          if (smart.hasOwnProperty('patient')) {
+              var patient = smart.patient;
+              var pt = patient.read();
 
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
+              var obv = smart.patient.api.fetchAll({
+                  type: 'Observation',
+                  query: {
                       code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                          $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
                               'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                       }
-                    }
-                  });
+                  }
+              });
 
-        $.when(pt, obv).fail(onError);
+              $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var gender = patient.gender;
-          var dob = new Date(patient.birthDate);
-          var day = dob.getDate();
-          var monthIndex = dob.getMonth() + 1;
-          var year = dob.getFullYear();
+              $.when(pt, obv).done(function(patient, obv) {
+                  var gender = patient.gender;
+                  var dob = new Date(patient.birthDate);
+                  var day = dob.getDate();
+                  var monthIndex = dob.getMonth() + 1;
+                  var year = dob.getFullYear();
 
-          var mrn=patient.identifier.find(i=>i.type.text=="MRN").value;
+                  var mrn=patient.identifier.find(i=>i.type.text=="MRN").value;
 
-          var dobStr = monthIndex + '/' + day + '/' + year;
-          var fname = '';
-          var lname = '';
+                  var dobStr = monthIndex + '/' + day + '/' + year;
+                  var fname = '';
+                  var lname = '';
 
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
+                  if (typeof patient.name[0] !== 'undefined') {
+                      fname = patient.name[0].given.join(' ');
+                      lname = patient.name[0].family.join(' ');
+                  }
+
+
+
+                  var p = defaultPatient();
+                  p.birthdate = dobStr;
+                  p.gender = gender;
+                  p.fname = fname;
+                  p.lname = lname;
+                  p.mrn=mrn;
+
+
+                  ret.resolve(p);
+              });
+          } else {
+              onError();
           }
-
-
-
-          var p = defaultPatient();
-          p.birthdate = dobStr;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.mrn=mrn;
-
-
-          ret.resolve(p);
-        });
-      } else {
-        onError();
       }
-    }
 
-    FHIR.oauth2.ready(onReady, onError);
-    return ret.promise();
+      FHIR.oauth2.ready(onReady, onError);
+      return ret.promise();
 
   };
+
+
 
   function defaultPatient(){
     return {
@@ -125,18 +127,13 @@
   }
 
   window.drawVisualization = function(p) {
-    $('#holder').show();
-    $('#loading').hide();
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#age').html(p.age);
-    $('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);
+      $('#holder').show();
+      $('#loading').hide();
+      $('#fname').html(p.fname);
+      $('#lname').html(p.lname);
+      $('#gender').html(p.gender);
+      $('#birthdate').html(p.birthdate);
+      $('#mrn').html(p.mrn);
   };
 
 })(window);
